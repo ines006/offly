697c1385bd7d663eeac54b97a90d0c3336fd8308
Celina Cruz <celinac0501@gmail.com>
2025-01-09 13:35:22 +0000

    correção de erros

--- projetoOffly/app/index.js
+++ projetoOffly/app/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import  Navbar from './components/navbar';
 import { CardProvider } from './components/shake/cardContext';
 import Login from './components/entrar/login'
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 
-export default function App() {
+export default function Index() {
+  const router = useRouter();
   return (
     <>
       <View style={styles.container}>

7d65ef0a226544b6b8955cb25dc3e6331653f049
flaviob4465 <flaviobrandao4465@gmail.com>
2025-01-16 01:35:15 +0000

    upload funcional porem o icon nao aparece

--- projetoOffly/app/components/uploadScreenTime/UploadScreen.js
+++ projetoOffly/app/components/uploadScreenTime/UploadScreen.js
@@ -1,119 +1,83 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Modal, Image, Alert } from "react-native";
-import { launchImageLibrary } from "react-native-image-picker";
+import * as ImagePicker from "expo-image-picker";
 import Svg, { Circle, Path } from "react-native-svg";
 import { useRouter } from "expo-router";
-
-// Firebase Imports
 import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../../firebase/firebaseApi";
 
 const UploadScreen = () => {
-
   const router = useRouter();
-  const [userId, setUserId] = useState(null); //var de estado que guarda o id do user logado
-  const [submitVisible, setSubmitVisible] = useState(false); // var de estado que define a visibilidade do botão "Submeter"
-  const [modalVisible, setModalVisible] = useState(false); // var de estado que define a visibilidade do modal de sucesso
-  const [selectedImage, setSelectedImage] = useState(null); // var de estado que guarda a imagem selecionada
+  const [userId, setUserId] = useState(null);
+  const [submitVisible, setSubmitVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-  // Verificação de utilizador logado
   useEffect(() => {
     const currentUser = auth.currentUser;
     if (currentUser) {
       setUserId(currentUser.uid);
     } else {
-      Alert.alert('Erro', 'Nenhum utilizador logado!');
+      Alert.alert("Erro", "Nenhum utilizador logado!");
     }
   }, []);
 
-  // Função para abrir o botão de submeter
-  const handleButton = () => {
-    setSubmitVisible(true);
-  };
-
-  // Função para abrir a modal e submeter dados de upload para a firebase
   const handleSubmit = async () => {
     setSubmitVisible(false);
     setModalVisible(true);
 
-    try { // Função para verificar e atualizar o Firestore
-    
+    try {
       if (userId) {
-      const userDocRef = doc(db, "users", userId); 
+        const userDocRef = doc(db, "users", userId);
+        const today = new Date().toISOString().split("T")[0];
 
-      const currentTime = Date.now(); // Timestamp UNIX
-      const today = new Date().toISOString().split("T")[0]; // Data no formato YYYY-MM-DD
-
-      // Verifica se o documento existe
-      const docSnap = await getDoc(userDocRef);
-
-      if (docSnap.exists()) {
-        // Se o documento existir, atualiza os campos
-        await updateDoc(userDocRef, {
-          upload: true,
-          data: today,
-        });
-        console.log("Campos atualizados no Firestore.");
-      } else {
-        // Se o documento não existir, cria o documento com os campos
-        await setDoc(userDocRef, {
-          upload: true,
-          data: today,
-        });
-        console.log("Documento criado no Firestore.");
+        const docSnap = await getDoc(userDocRef);
+        if (docSnap.exists()) {
+          await updateDoc(userDocRef, { upload: true, data: today });
+        } else {
+          await setDoc(userDocRef, { upload: true, data: today });
+        }
       }
-    }
     } catch (error) {
       console.error("Erro ao registar upload:", error);
     }
   };
 
-  // Função para fechar a modal
-  const handleCloseModal = () => {
-    setModalVisible(false);
-    router.push("../pag-principal-lg/pag-principal-lg");
-  };
+  const handleSelectImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("Erro", "Permissão para acessar a galeria negada!");
+      return;
+    }
 
-  // Função para abrir a galeria e selecionar uma imagem
-  const handleSelectImage = () => {
-    const options = {
-      mediaType: "photo",
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
       quality: 1,
-    };
-
-    launchImageLibrary(options, (response) => {
-      if (response.didCancel) {
-        console.log("Seleção de imagem cancelada");
-      } else if (response.errorCode) {
-        console.error("Erro ao selecionar imagem:", response.errorMessage);
-      } else {
-        const uri = response.assets[0]?.uri;
-        setSelectedImage(uri);
-        setSubmitVisible(true);
-      }
     });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setSubmitVisible(true);
+    }
   };
 
-  // Função para remover a imagem
   const handleRemoveImage = () => {
     setSelectedImage(null);
     setSubmitVisible(false);
   };
 
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    router.push("../pag-principal-lg/pag-principal-lg");
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-      {/* Botão de Voltar atrás */}
-      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
-          <Svg width={36} height={35} viewBox="0 0 36 35" fill="none">
-            <Circle
-              cx="18.1351"
-              cy="17.1713"
-              r="16.0177"
-              stroke="#263A83"
-              strokeWidth={2}
-            />
+        <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+          <Svg width={36} height={35} viewBox="0 0 36 35" fill="none" style={{ borderWidth: 1, borderColor: 'red' }}>
+            <Circle cx="18.1351" cy="17.1713" r="16.0177" stroke="#263A83" strokeWidth={2} />
             <Path
               d="M21.4043 9.06396L13.1994 16.2432C12.7441 16.6416 12.7441 17.3499 13.1994 17.7483L21.4043 24.9275"
               stroke="#263A83"
@@ -122,41 +86,18 @@ const UploadScreen = () => {
             />
           </Svg>
         </TouchableOpacity>
-        {/* Título */}
         <Text style={styles.title}>Upload do tempo de ecrã</Text>
       </View>
 
       <View style={styles.dashedBox}>
-        {/* Mostra a imagem selecionada ou o ícone de upload */}
         {selectedImage ? (
           <Image source={{ uri: selectedImage }} style={styles.imagePreview} />
         ) : (
-          <>
           <TouchableOpacity onPress={handleSelectImage}>
-            <Svg width={49} height={49} viewBox="0 0 49 49" fill="none">
-            <Path
-              d="M0.125 8.25C0.125 3.7627 3.7627 0.125 8.25 0.125H40.75C45.2374 0.125 48.875 3.7627 48.875 8.25V24.559C47.2351 23.509 45.4094 22.7236 43.4583 22.2626V8.25C43.4583 6.75424 42.2458 5.54167 40.75 5.54167H8.25C6.75424 5.54167 5.54167 6.75424 5.54167 8.25V40.75C5.54167 42.2458 6.75424 43.4583 8.25 43.4583H22.2626C22.7236 45.4094 23.509 47.2351 24.559 48.875H8.25C3.7627 48.875 0.125 45.2374 0.125 40.75V8.25Z"
-              fill="#263A83"
-            />
-          </Svg>
-          <Svg
-            width={31}
-            height={31}
-            viewBox="0 0 31 31"
-            fill="none"
-            style={styles.plusIcon}
-          >
-            <Path
-              d="M30.2917 15.3958C30.2917 7.169 23.6227 0.5 15.3958 0.5C7.169 0.5 0.5 7.169 0.5 15.3958C0.5 23.6227 7.169 30.2917 15.3958 30.2917C23.6227 30.2917 30.2917 23.6227 30.2917 15.3958ZM16.7516 16.75L16.753 23.5303C16.753 24.2784 16.1469 24.8845 15.3988 24.8845C14.651 24.8845 14.0446 24.2784 14.0446 23.5303L14.0433 16.75H7.26027C6.5125 16.75 5.9061 16.1439 5.9061 15.3958C5.9061 14.6481 6.5125 14.0417 7.26027 14.0417H14.043L14.0417 7.26894C14.0417 6.5209 14.6481 5.91477 15.3958 5.91477C16.1436 5.91477 16.75 6.5209 16.75 7.26894L16.7514 14.0417H23.529C24.277 14.0417 24.8831 14.6481 24.8831 15.3958C24.8831 16.1439 24.277 16.75 23.529 16.75H16.7516Z"
-              fill="#263A83"
-            />
-          </Svg>
+            <Text style={styles.description}>Faz upload do print do tempo de ecrã</Text>
           </TouchableOpacity>
-          <Text style={styles.description}>Faz upload do print do tempo de ecrã</Text>
-          </>
         )}
-        
-        {/* Botão "Remover" */}
+
         {selectedImage && (
           <TouchableOpacity style={styles.removeButton} onPress={handleRemoveImage}>
             <Text style={styles.removeText}>Remover</Text>
@@ -164,51 +105,27 @@ const UploadScreen = () => {
         )}
       </View>
 
-      {/* Botão "Submeter" */}
       {submitVisible && (
         <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
           <Text style={styles.submitText}>Submeter</Text>
         </TouchableOpacity>
       )}
 
-      {/* Modal de sucesso */}
       <Modal
         visible={modalVisible}
-        animationType="none" 
+        animationType="none"
         transparent={true}
         onRequestClose={handleCloseModal}
-        >
+      >
         <View style={styles.modalOverlay}>
-            <View style={styles.modalContainer}>
-            {/* Círculo com o ícone no topo */}
-            <View style={styles.circleWrapper}>
-            <Svg width={80} height={80} viewBox="0 0 80 80" fill="none">
-            <Circle cx="40" cy="40" r="40" fill="#BFE0FF" />
-            <Path
-                d="M33.6 51.2L24 41.6C23.1 40.7 23.1 39.1 24 38.2C24.9 37.3 26.5 37.3 27.4 38.2L34.8 45.6L52.6 27.8C53.5 26.9 55.1 26.9 56 27.8C56.9 28.7 56.9 30.3 56 31.2L36 51.2C35.1 52.1 33.9 52.1 33.6 51.2Z"
-                fill="#263A83"
-            />
-            </Svg>
-            </View>
-
-            {/* Conteúdo da modal */}
-            <Text style={styles.modalTitle}>Print do tempo de ecrã submetido com sucesso</Text>
-            <Text style={styles.modalMessage}>
-                Poderás perder um máximo de 25 pontos.
-            </Text>
-            <Text style={styles.modalMessage2}>
-                Amanhã será possível realizar outra submissão!
-            </Text>
-            <TouchableOpacity
-                style={styles.modalButton}
-                onPress={handleCloseModal}
-                
-            >
-                <Text style={styles.modalButtonText}>Fechar</Text>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Upload submetido com sucesso!</Text>
+            <TouchableOpacity style={styles.modalButton} onPress={handleCloseModal}>
+              <Text style={styles.modalButtonText}>Fechar</Text>
             </TouchableOpacity>
-            </View>
+          </View>
         </View>
-        </Modal>
+      </Modal>
     </View>
   );
 };
@@ -230,6 +147,7 @@ const styles = StyleSheet.create({
   backButton: {
     position: "absolute",
     left: 25,
+    zIndex: 10, 
   },
   title: {
     width: 172,

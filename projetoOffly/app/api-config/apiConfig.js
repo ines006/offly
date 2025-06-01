b190e202814f3e2e99cfbe95745ebc705bce7ea6
Celina Cruz <celinac0501@gmail.com>
2025-06-01 18:59:52 +0100

    para fazer o merge

--- projetoOffly/app/api-config/apiConfig.js
+++ projetoOffly/app/api-config/apiConfig.js
@@ -1,2 +1,2 @@
 export const baseurl =
-  "https://a3f7-2001-818-e798-4a00-1d8c-ec53-2515-8934.ngrok-free.app"; // URL que varia conforme a indica pelo NGROk
+  "https://b6c7-2001-8a0-ee96-4600-14ee-5d8e-17de-f81d.ngrok-free.app"; // URL que varia conforme a indica pelo NGROk

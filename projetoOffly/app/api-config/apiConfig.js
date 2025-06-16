6498b8cee994cae2e9bf221f5ee345536fb34bdc
flaviob4465 <flaviobrandao4465@gmail.com>
2025-06-16 16:13:27 +0100

    push

--- projetoOffly/app/api-config/apiConfig.js
+++ projetoOffly/app/api-config/apiConfig.js
@@ -1,4 +1,4 @@
 export const baseurl =
-  "https://741b-2001-8a0-ee9c-3b00-c187-600b-5c2a-5fcd.ngrok-free.app"; // URL que varia conforme a indica pelo NGROk
+  "https://5bb9-2001-8a0-ee9c-3b00-c187-600b-5c2a-5fcd.ngrok-free.app"; // URL que varia conforme a indica pelo NGROk
 
   

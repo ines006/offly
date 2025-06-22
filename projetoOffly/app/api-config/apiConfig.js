bc84a59827af632b90c6fc48e36fccdc670f049f
Celina Cruz <celinac0501@gmail.com>
2025-06-22 05:10:01 +0100

    .

--- projetoOffly/app/api-config/apiConfig.js
+++ projetoOffly/app/api-config/apiConfig.js
@@ -1,4 +1,4 @@
 export const baseurl =
-  "https://a44d-2001-8a0-ee84-7100-ed04-5720-a9fc-bd2c.ngrok-free.app"; // URL que varia conforme a indica pelo NGROk
+  "https://a290-2001-8a0-ee84-7100-ed04-5720-a9fc-bd2c.ngrok-free.app"; // URL que varia conforme a indica pelo NGROk
 
 
